2532150907d1387d416e8fb4b5c8758e159718a2
MarcioBittencourt <marciobitt97@gmail.com>
2021-07-31 23:53:41 -0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,28 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployerModule } from './employer/employer.module';
-import { CompanyModule } from './company/company.module';
-import { AssessementModule } from './assessement/assessement.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
-import { DatabaseModule } from './database/database.module';
-import { AssessementsModule } from './assessements/assessements.module';
-import { EmployerModule } from './employer/employer.module';
-import { AssessementsModule } from './assessements/assessements.module';
 
 @Module({
   imports: [
     EmployerModule,
-    CompanyModule,
-    AssessementModule,
-    DatabaseModule,
     TypeOrmModule.forRootAsync({
       useFactory: async () =>
         Object.assign(await getConnectionOptions(), {
           autoLoadEntities: true,
         }),
     }),
-    AssessementsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
