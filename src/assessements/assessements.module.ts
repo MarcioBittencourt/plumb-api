77ce154817c78c3207f33cd59617f9403e8d14ef
MarcioBittencourt <marciobitt97@gmail.com>
2021-08-04 18:57:44 -0300

    implementation the typeOrm configuration

--- src/assessements/assessements.module.ts
+++ src/assessements/assessements.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AssessementsService } from './assessements.service';
 import { AssessementsController } from './assessements.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AssessementRepository } from './assessement.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AssessementRepository])],
   controllers: [AssessementsController],
   providers: [AssessementsService],
 })
