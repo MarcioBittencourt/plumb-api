47c6300bbffbfc9043931656a8a4a07f1e34d8b2
MarcioBittencourt <marciobitt97@gmail.com>
2021-08-04 18:49:06 -0300

    addition of assessementsModule on import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,20 @@ import { AppService } from './app.service';
 import { EmployerModule } from './employer/employer.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
+import { QuestionsModule } from './questions/questions.module';
+import { AssessementsModule } from './assessements/assessements.module';
 
 @Module({
   imports: [
     EmployerModule,
+    AssessementsModule,
     TypeOrmModule.forRootAsync({
       useFactory: async () =>
         Object.assign(await getConnectionOptions(), {
           autoLoadEntities: true,
         }),
     }),
+    QuestionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
