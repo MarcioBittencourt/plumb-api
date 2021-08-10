929a94f6abfdad076ea1a067bf23027665ac7128
MarcioBittencourt <marciobitt97@gmail.com>
2021-08-10 17:04:46 -0300

    refactoring of the employees module

--- src/employer/dto/create-employer.dto.ts
+++ src/employer/dto/create-employer.dto.ts
@@ -1,4 +1,8 @@
+import { IsString } from 'class-validator';
+
 export class CreateEmployerDto {
+  @IsString()
   name: string;
+  @IsString()
   role: string;
 }
--- src/employer/employer.controller.ts
+++ src/employer/employer.controller.ts
@@ -10,36 +10,45 @@ import {
 import { EmployerService } from './employer.service';
 import { CreateEmployerDto } from './dto/create-employer.dto';
 import { UpdateEmployerDto } from './dto/update-employer.dto';
+import { Employer } from './entities/employer.entity';
+import { UpdateResult } from 'typeorm';
 
 @Controller('employer')
 export class EmployerController {
   constructor(private readonly employerService: EmployerService) {}
 
   @Post()
-  create(@Body() createEmployerDto: CreateEmployerDto) {
-    return this.employerService.create(createEmployerDto);
+  async create(
+    @Body() createEmployerDto: CreateEmployerDto,
+  ): Promise<Employer> {
+    const employer = await this.employerService.create(createEmployerDto);
+    return employer;
   }
 
   @Get()
-  findAll() {
-    return this.employerService.findAll();
+  async findAll(): Promise<Employer[]> {
+    const employer = await this.employerService.findAll();
+    return employer;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.employerService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Employer> {
+    const employer = await this.employerService.findOne(+id);
+    return employer;
   }
 
   @Patch(':id')
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updateEmployerDto: UpdateEmployerDto,
-  ) {
-    return this.employerService.update(+id, updateEmployerDto);
+  ): Promise<string> {
+    const employer = await this.employerService.update(+id, updateEmployerDto);
+    return employer;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.employerService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const employer = await this.employerService.remove(+id);
+    return employer;
   }
 }
