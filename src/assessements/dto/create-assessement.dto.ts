15d855790f59af8b04b6c3737433ba233eb28c7a
MarcioBittencourt <marciobitt97@gmail.com>
2021-08-12 11:27:45 -0300

    implementation of typeorm decorators

--- src/assessements/dto/create-assessement.dto.ts
+++ src/assessements/dto/create-assessement.dto.ts
@@ -1,15 +1,19 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateAssessementDto {
   @IsNumber()
+  @IsNotEmpty()
   evaluator: number;
 
   @IsNumber()
+  @IsNotEmpty()
   rated: number;
 
   @IsString()
+  @IsNotEmpty()
   description: string;
 
   @IsNumber()
+  @IsNotEmpty()
   punctuation: number;
 }
--- src/assessements/dto/update-assessement.dto.ts
+++ src/assessements/dto/update-assessement.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty } from 'class-validator';
 import { CreateAssessementDto } from './create-assessement.dto';
 
-export class UpdateAssessementDto extends PartialType(CreateAssessementDto) {}
+export class UpdateAssessementDto extends PartialType(CreateAssessementDto) {
+  @IsNotEmpty()
+  id: string;
+}
