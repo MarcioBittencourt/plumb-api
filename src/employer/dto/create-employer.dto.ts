ce1eee67f4b00c0324fdcfe90c19259260bb91d7
MarcioBittencourt <marciobitt97@gmail.com>
2021-08-12 11:27:57 -0300

    implementation of typeorm decorators

--- src/employer/dto/create-employer.dto.ts
+++ src/employer/dto/create-employer.dto.ts
@@ -1,8 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateEmployerDto {
   @IsString()
+  @IsNotEmpty()
   name: string;
+
   @IsString()
+  @IsNotEmpty()
   role: string;
 }
--- src/employer/dto/update-employer.dto.ts
+++ src/employer/dto/update-employer.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty } from 'class-validator';
 import { CreateEmployerDto } from './create-employer.dto';
 
-export class UpdateEmployerDto extends PartialType(CreateEmployerDto) {}
+export class UpdateEmployerDto extends PartialType(CreateEmployerDto) {
+  @IsNotEmpty()
+  id: string;
+}
