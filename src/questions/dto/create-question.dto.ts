48218a44e768cda6d36ebdf90efb416f53623bfd
MarcioBittencourt <marciobitt97@gmail.com>
2021-08-12 11:28:13 -0300

    implementation of typeorm decorators

--- src/questions/dto/create-question.dto.ts
+++ src/questions/dto/create-question.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateQuestionDto {
+  @IsString()
+  @IsNotEmpty()
   category: string;
+
+  @IsString()
+  @IsNotEmpty()
   content: string;
+
+  @IsString()
+  @IsNotEmpty()
   typeQuestion: string;
 }
