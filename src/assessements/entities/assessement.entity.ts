352778c2d3bd047d96bccd9adaa18cdca9da8f8a
MarcioBittencourt <marciobitt97@gmail.com>
2021-08-13 17:32:45 -0300

    employee module name correction

--- src/assessements/entities/assessement.entity.ts
+++ src/assessements/entities/assessement.entity.ts
@@ -1,9 +1,12 @@
-import { Employer } from 'src/employer/entities/employer.entity';
+import { Employee } from 'src/employees/entities/employee.entity';
+import { Question } from 'src/questions/entities/question.entity';
 import {
   Column,
   Entity,
+  Generated,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -12,22 +15,29 @@ export class Assessement {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @ManyToOne((type) => Employer, (evaluator) => evaluator.evaluations)
+  @Column({ name: 'uuid', type: 'uuid' })
+  @Generated('uuid')
+  uuid: string;
+
+  @ManyToOne((type) => Employee, (evaluator) => evaluator.evaluations)
   @JoinColumn({
     name: 'evaluator_id',
     referencedColumnName: 'id',
   })
-  evaluator: Employer | number;
+  evaluator: Employee | number;
 
-  @ManyToOne((type) => Employer, (rated) => rated.ratings)
+  @ManyToOne((type) => Employee, (rated) => rated.ratings)
   @JoinColumn({
     name: 'rated_id',
     referencedColumnName: 'id',
   })
-  rated: Employer | number;
+  rated: Employee | number;
 
   @Column({ type: 'varchar' })
   description: string;
+
+  @OneToMany((type) => Question, (question) => question.assessement)
+  questions: Question[];
 
   @Column({ type: 'int' })
   punctuation: number;
